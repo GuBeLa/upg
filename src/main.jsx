bec45eb98869517ad20eccacef36b3af5e21a28a
Temur Gogitidze <tgogitidze@ip.local>
2024-08-19 18:16:28 +0400

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,11 @@
-// src/main.jsx
-import { createRoot } from "react-dom/client";
+import * as ReactDOM from "react-dom/client";
 import { QueryProvider } from "@/lib/react-query/QueryProvider";
 import { AppRouter } from "@/router/index";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryProvider>
     <AppRouter />
     {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
   </QueryProvider>
 );
-
-if (import.meta.hot) {
-  import.meta.hot.dispose(() => root.unmount());
-}

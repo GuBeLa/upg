3b302dfb0fa3fbd524b80635f4a379af6c690607
ირაკლი გუბელიძე <irakligubelidze@gmail.com>
2024-08-20 14:04:39 +0400

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
-import { createRoot } from "react-dom/client";
-import { StrictMode } from "react";
+import * as ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { QueryProvider } from "@/lib/react-query/QueryProvider";
-// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Router } from "./router/index";
+import { routes } from "@/router/index";
+import { Routes, Route } from "react-router-dom";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-
-root.render(
-  <StrictMode>
-    <QueryProvider>
-      <Router />
-    </QueryProvider>
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <QueryProvider>
+    <BrowserRouter>
+      {/ <AppRouter /> /}
+      <Routes>
+        {routes.map((route) => (
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
+      </Routes>
+    </BrowserRouter>
+    {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+  </QueryProvider>
 );
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,88 +1,76 @@
-import { Suspense, createElement } from "react";
+import { Suspense } from "react";
 import {
   createBrowserRouter,
   Navigate,
   RouterProvider,
 } from "react-router-dom";
 import CircularLoader from "@/components/CircularLoader";
+import QaMonitoringLayout from "@/layouts/QaMonitoringLayout";
 import { RootError } from "@/components/error";
 import BaseLayout from "@/layouts/BaseLayout";
-import MainLayout from "@/layouts/QaMonitoringLayout";
 
-export const router = createBrowserRouter([
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <BaseLayout />,
+//     errorElement: <RootError />,
+//     children: [
+//       {
+//         path: "qamonitoring",
+//         element: <QaMonitoringLayout />,
+//         children: [
+//           {
+//             index: true,
+//             path: "",
+//             element: (
+//               <Navigate
+//                 to={`${
+//                   import.meta.env.VITE_APP_ROUTE_PREFIX
+//                 }/qamonitoring/questionnaire`}
+//                 replace
+//               />
+//             ),
+//           },
+//           {
+//             path: "questionnaire",
+//             lazy: () => import("@/pages/qamonitoring/Questionnaire"),
+//           },
+//           {
+//             path: "setup",
+//             lazy: () => import("@/pages/qamonitoring/QuestionnaireSetup"),
+//           },
+//           {
+//             path: "review",
+//             lazy: () => import("@/pages/qamonitoring/Review"),
+//           },
+//           {
+//             path: "reports",
+//             lazy: () => import("@/pages/qamonitoring/Reports"),
+//           },
+//         ],
+//       },
+//     ].map((el) => ({
+//       ...el,
+//       path: `${import.meta.env.VITE_APP_ROUTE_PREFIX + "/" + el.path}`,
+//     })),
+//   },
+// ]);
+
+// console.log(router, "ROUTES");
+
+export const routes = [
   {
-    path: "",
-    element: <MainLayout />,
-    errorElement: <RootError />,
-    children: [
-      {
-        index: true,
-        element: <Navigate to="/setup" replace />,
-        path: "",
-      },
-      {
-        path: "questionnaire",
-        lazy: () => import("@/pages/qamonitoring/Questionnaire"),
-      },
-      {
-        path: "setup",
-        lazy: () => import("@/pages/qamonitoring/QuestionnaireSetup"),
-      },
-      {
-        path: "review",
-        lazy: () => import("@/pages/qamonitoring/Review"),
-      },
-      {
-        path: "reports",
-        lazy: () => import("@/pages/qamonitoring/Reports"),
-      },
-    ].map((el) => ({
-      ...el,
-      path: `${import.meta.env.VITE_APP_ROUTE_PREFIX + "/" + el.path}`,
-    })),
+    path: "/home",
+    element: <div>Home</div>,
   },
   {
-    path: "",
-    element: <MainLayout />,
-    errorElement: <RootError />,
-    children: [
-      {
-        index: true,
-        element: <Navigate to="/dashboard/setup" replace />,
-        path: "",
-      },
-      {
-        path: "/dashboard/questionnaire",
-        lazy: () => import("@/pages/qamonitoring/Questionnaire"),
-      },
-      {
-        path: "/dashboard/setup",
-        lazy: () => import("@/pages/qamonitoring/QuestionnaireSetup"),
-      },
-      {
-        path: "/dashboard/review",
-        lazy: () => import("@/pages/qamonitoring/Review"),
-      },
-      {
-        path: "/dashboard/reports",
-        lazy: () => import("@/pages/qamonitoring/Reports"),
-      },
-    ].map((el) => ({
-      ...el,
-      path: `${import.meta.env.VITE_APP_ROUTE_PREFIX + "/" + el.path}`,
-    })),
+    path: "/nohome",
+    element: <div>No home</div>,
   },
-]);
-
-console.log(router, "router");
-
-export function Router() {
-  return createElement(RouterProvider, { router });
-}
-
-if (import.meta.hot) {
-  import.meta.hot.dispose(() => router.dispose());
-}
+].map((el) => ({
+  ...el,
+  path: import.meta.env.VITE_APP_ROUTE_PREFIX + "/" + el.path,
+}));
 
 // export function AppRouter() {
 //   return (

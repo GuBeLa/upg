1f4a243667ab43a89bfb2ce5521ec427e8f57440
ირაკლი გუბელიძე <irakligubelidze@gmail.com>
2024-08-20 13:13:54 +0400

    map

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,62 +1,64 @@
-import { Suspense, createElement } from "react";
+import { Suspense } from "react";
 import {
   createBrowserRouter,
   Navigate,
   RouterProvider,
 } from "react-router-dom";
 import CircularLoader from "@/components/CircularLoader";
+import QaMonitoringLayout from "@/layouts/QaMonitoringLayout";
 import { RootError } from "@/components/error";
 import BaseLayout from "@/layouts/BaseLayout";
-import MainLayout from "@/layouts/QaMonitoringLayout";
 
-export const router = createBrowserRouter([
+const router = createBrowserRouter([
   {
-    path: "/qamonitoring",
-    element: <MainLayout />,
+    path: "/",
+    element: <BaseLayout />,
     errorElement: <RootError />,
     children: [
       {
-        index: true,
-        element: <Navigate to="/qamonitoring/setup" replace />,
-        path: "",
+        path: "qamonitoring",
+        element: <QaMonitoringLayout />,
+        children: [
+          {
+            index: true,
+            path: "",
+            element: (
+              <Navigate
+                to={`${
+                  import.meta.env.VITE_APP_ROUTE_PREFIX
+                }/qamonitoring/questionnaire`}
+                replace
+              />
+            ),
+          },
+          {
+            path: "questionnaire",
+            lazy: () => import("@/pages/qamonitoring/Questionnaire"),
+          },
+          {
+            path: "setup",
+            lazy: () => import("@/pages/qamonitoring/QuestionnaireSetup"),
+          },
+          {
+            path: "review",
+            lazy: () => import("@/pages/qamonitoring/Review"),
+          },
+          {
+            path: "reports",
+            lazy: () => import("@/pages/qamonitoring/Reports"),
+          },
+        ],
       },
-      {
-        path: "questionnaire",
-        lazy: () => import("@/pages/qamonitoring/Questionnaire"),
-      },
-      {
-        path: "setup",
-        lazy: () => import("@/pages/qamonitoring/QuestionnaireSetup"),
-      },
-      {
-        path: "review",
-        lazy: () => import("@/pages/qamonitoring/Review"),
-      },
-      {
-        path: "reports",
-        lazy: () => import("@/pages/qamonitoring/Reports"),
-      },
-    ].map((el) => ({
-      ...el,
-      path: `${import.meta.env.VITE_APP_ROUTE_PREFIX + "/" + el.path}`,
-    })),
+    ],
   },
 ]);
 
-console.log(router, "router");
+console.log(router, "ROUTES");
 
-export function Router() {
-  return createElement(RouterProvider, { router });
+export function AppRouter() {
+  return (
+    <Suspense fallback={<CircularLoader absolutePosition={true} />}>
+      <RouterProvider router={router} />
+    </Suspense>
+  );
 }
-
-if (import.meta.hot) {
-  import.meta.hot.dispose(() => router.dispose());
-}
-
-// export function AppRouter() {
-//   return (
-//     <Suspense fallback={<CircularLoader absolutePosition={true} />}>
-//       <RouterProvider router={router} />
-//     </Suspense>
-//   );
-// }
